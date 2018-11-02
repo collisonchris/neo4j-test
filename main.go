d4ee661644defe9feb5bc843236728548ba607ab
Chris Collison <collisonchris@gmail.com>
2018-11-02 14:05:04 -0500

    adding highly concurrent example

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-	log "github.com/sirupsen/logrus"
+
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 	boltLogs "github.com/johnnadratowski/golang-neo4j-bolt-driver/log"
-
+	log "github.com/sirupsen/logrus"
 )
 
 type NeoClient struct {
@@ -15,7 +15,7 @@ type NeoClient struct {
 }
 
 func NewNeo4jClient(hostname string, port string, user string, password string) (NeoClient, error) {
-	pool, err := bolt.NewDriverPool("bolt://"+user+":"+password+"@"+hostname+":"+port, 10)
+	pool, err := bolt.NewDriverPool("bolt://"+user+":"+password+"@"+hostname+":"+port, 5)
 	boltLogs.SetLevel("info")
 	return NeoClient{pool}, err
 }
@@ -31,26 +31,34 @@ func getConnectionFromPool(pool bolt.DriverPool) bolt.Conn {
 var neoClient NeoClient
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(w, "heartbeat")
+	fmt.Println(w, "heartbeat")
 }
 
 func main() {
-	go aliveQuery()
+	neoClient, _ := NewNeo4jClient("localhost", "7687", "neo4j", "")
+	ping(neoClient)
+	for i := 0; i < 10; i++ {
+		go aliveTask(neoClient)
+	}
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
-func aliveQuery() {
-	neoClient, err := NewNeo4jClient("localhost", "7687", "neo4j", "neo4j1")
-	for range time.Tick(time.Second * 2 ) {
-        conn := getConnectionFromPool(neoClient.Pool)
-		_, err = conn.ExecNeo("return 1", nil)
-		if(err == nil) {
-			log.Info("healthy ping")
-		} else {
-			log.WithError(err).Error("error with health ping")
-		}
-		conn.Close()
-    }
-	
+func aliveTask(neoClient NeoClient) {
+	for range time.Tick(time.Minute * 60) {
+		ping(neoClient)
+	}
+
+}
+
+func ping(neoClient NeoClient) {
+	conn := getConnectionFromPool(neoClient.Pool)
+	_, err := conn.ExecNeo("return 1", nil)
+	if err == nil {
+		log.Info("healthy ping")
+	} else {
+		log.WithError(err).Error("error with health ping")
+	}
+	conn.Close()
 }
